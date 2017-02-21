ef67e5a916ca17030bbd10f80b6b2917fb45614d
Seth Battis <seth@battis.net>
2017-02-20 20:13:31 -0500

    Theoretical autonomous

--- MecanumDriveJan21/src/org/usfirst/frc3566/MecanumDriveJan21/commands/AutonomousLiftSide.java
+++ MecanumDriveJan21/src/org/usfirst/frc3566/MecanumDriveJan21/commands/AutonomousLiftSide.java
@@ -12,54 +12,64 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class AutonomousLiftSide extends Command {
-	
-	private Direction leftOrRightTarget; //1 = left; 2 = right
+
+	private Direction leftOrRightTarget; // 1 = left; 2 = right
 	private boolean ready, done;
-	//this boolean shows if the auto command is aware of the targets
-	
-    public AutonomousLiftSide(Direction side) {
-    	leftOrRightTarget = side;
-    	ready = false;
-    	done = false;
-    }
+	// this boolean shows if the auto command is aware of the targets
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	//make the robot drive forward for 14 feet to be right next to the target!!!
-    	new DriveForDistance(Direction.FORWARD, 2, 0.2).start(); //direction, distance, speed
-    	Timer.delay(2);
-    	ready = true;
-    }
+	public AutonomousLiftSide(Direction side) {
+		leftOrRightTarget = side;
+		ready = false;
+		done = false;
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	//execute has to happen AFTER initialize is done. Check if it does.
-    	if (ready){
-    		//if no targets in view yet, rotate a little bit and then check again
-    		if(leftOrRightTarget == Direction.LEFT){
-    	    	Robot.mecanumDriveTrain.rotateRight(0.4);
-    	    	}else if(leftOrRightTarget == Direction.RIGHT){
-    	        	Robot.mecanumDriveTrain.rotateLeft(0.4);
-    	    	}
-    	//	targetsDetected = FishyThread.checkIfTargetsDetected();
-    	}else{
-    	
-    	}
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		// make the robot drive forward for 14 feet to be right next to the
+		// target!!!
+		new DriveForDistance(Direction.FORWARD, 2, 0.2).start(); // direction,
+																	// distance,
+																	// speed
+		Timer.delay(2);
+		ready = true;
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		// execute has to happen AFTER initialize is done. Check if it does.
+		if (ready) {
+			/*
+			 * if no targets in view yet, rotate a little bit and then check
+			 * again
+			 */
+			if (!FishyCam.isTargetsDetected()) {
+				if (leftOrRightTarget == Direction.LEFT) {
+					Robot.mecanumDriveTrain.rotateRight(0.4);
+				} else if (leftOrRightTarget == Direction.RIGHT) {
+					Robot.mecanumDriveTrain.rotateLeft(0.4);
+				}
+			} else {
+				// get lined up and deliver that gear!
+				new AutonomousLiftFront().start();
+			}
+		} else {
+			// do nothing (wait!)
+		}
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	Robot.mecanumDriveTrain.stopDriveTrain();
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	end();
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.mecanumDriveTrain.stopDriveTrain();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
 }
