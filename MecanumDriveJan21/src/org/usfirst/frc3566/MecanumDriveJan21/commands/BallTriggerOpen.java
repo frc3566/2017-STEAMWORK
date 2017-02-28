cd5976c58c0da87357767f0aae7017f57e884c1c
Gillian Yue <gillianyue1999@hotmail.com>
2017-02-28 09:13:45 -0500

    One more TODO

--- MecanumDriveJan21/src/org/usfirst/frc3566/MecanumDriveJan21/commands/BallTriggerOpen.java
+++ MecanumDriveJan21/src/org/usfirst/frc3566/MecanumDriveJan21/commands/BallTriggerOpen.java
@@ -4,36 +4,36 @@ import org.usfirst.frc3566.MecanumDriveJan21.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
+/* 
+ * FIXME We need to wait to open the ball trigger until the shooter is at full speed, which means we need to connect to the CIMcoder and get a reading
  */
 public class BallTriggerOpen extends Command {
-	
-    public BallTriggerOpen() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	Robot.shooter.openTrigger();
-    }
+	public BallTriggerOpen() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		Robot.shooter.openTrigger();
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
